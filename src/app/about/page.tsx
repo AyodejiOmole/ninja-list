ca91c60d2eabfeb012aef609395aeba0220f5c22
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-22 09:25:46 +0100

    added filler text to about ninja page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About Ninja",
+    description: "an about us page for Ninja Listing"
+};
+
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='text-center lg:p-10 md:p-10 py-10 px-2'>
+            <h1 className='text-xl my-10 font-extrabold font-mono'>About</h1>
+            <p className="font-mono text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <p className="font-mono text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+            <Link href="/ninja-listing">
+                <div className="my-6 p-2 lg:w-[200px] md:w-[200px] hover:text-white text-black font-mono w-full mx-auto bg-white hover:border-2 rounded hover:border-white hover:bg-black">
+                    See the Ninjas
+                </div>
+            </Link>
         </div>
     )
 }
