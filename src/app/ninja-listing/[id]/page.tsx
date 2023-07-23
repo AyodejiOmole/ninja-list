059cc442d2557dc4a6b1ed3e10f3b31d4f2d5f23
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-23 21:19:54 +0100

    final adjustments on details page

--- src/app/ninja-listing/[id]/page.tsx
+++ src/app/ninja-listing/[id]/page.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
+import getUser from '@/lib/getUser';
+import { Suspense } from 'react';
 
-// export async function generateStaticParams() {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-//   const data = await res.json();
-
-//   const paths = data.map((user: UserType) => ({
-//     params: { id: user.id.toString() },
-//   }));
-
-//   // const hostPath = profiles?.map((profile) => ({
-//   //   params: {host: profile.username},
-//   // }))
-
-//   return {
-//     paths,
-//   } 
-// };
+type Params = {
+  params: {
+    id: number
+  }
+}
 
 async function getPost(params: { id: number } ) {
   const res = await fetch('https://jsonplaceholder.typicode.com/users/' + params.id);
@@ -24,15 +15,19 @@ async function getPost(params: { id: number } ) {
   return data;
 };
 
-const Ninja = async ({ params }: { params: { id: number }} ) => {
-  const ninja: UserType = await getPost(params);
+const Ninja = async ({ params }: Params ) => {
+  const data: Promise<UserType> = getUser(params.id);
+  const ninja = await data;
+
   return (
     <div className='mt-10 w-full flex justify-center'>
       <div className="mx-auto">
-        <h1 className='font-mono mt-3 text-xl font-extrabold'>{ ninja.name }</h1>
-        <p className='font-mono mt-3 text-base font-normal'>{ ninja.email }</p>
-        <p className='font-mono mt-3 text-base font-normal'>{ ninja.website }</p>
-        <p className='font-mono mt-3 text-base font-normal'>{ ninja.address.city }</p>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <h1 className='font-mono mt-3 text-xl font-extrabold'>{ ninja.name }</h1>
+          <p className='font-mono mt-3 text-base font-normal'>{ ninja.email }</p>
+          <p className='font-mono mt-3 text-base font-normal'>{ ninja.website }</p>
+          <p className='font-mono mt-3 text-base font-normal'>{ ninja.address.city }</p>
+        </Suspense>
       </div>
     </div>
   )
