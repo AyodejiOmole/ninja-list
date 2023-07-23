fd2c5697a1fca5701e2c86f9b53aa3e9832bb2ed
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-23 13:26:31 +0100

    added types to data gotten from jsonplaceholder.com

--- src/app/ninja-listing/page.tsx
+++ src/app/ninja-listing/page.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import type { Metadata } from 'next';
 
+interface UserType {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: {
+      street: string,
+      suite: string,
+      city: string,
+      zipcode: string,
+      geo: {
+          lat: string,
+          lng: string
+      }
+  },
+  phone: string,
+  website: string,
+  company: {
+      name: string,
+      catchPhrase: string,
+      bs: string
+  }
+}
+
 export const metadata: Metadata = {
   title: "Ninja Listing | Home"
 }
 
-const NinjaListing = () => {
+export const getUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: 'force-cache' });
+  return res.json();
+}
+
+const NinjaListing = async () => {
+  const data: UserType[] = await getUsers();
+
+  if(!data) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
-      
+      {data.map((user: UserType, index: number) => {
+        return (
+          <div key={index}>
+            <p>{user.website}</p>
+            {/* <p>{user}</p> */}
+          </div>
+        )
+      })}
     </div>
   )
 }
