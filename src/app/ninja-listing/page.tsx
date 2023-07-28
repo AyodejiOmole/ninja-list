86ce3c4cbbe814d68eee8cb72d1d6b0954dcb507
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-28 23:14:22 +0100

    removed invalid export type in ninja-listing/page.tsx file

--- src/app/ninja-listing/page.tsx
+++ src/app/ninja-listing/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: "Ninja Listing | Home"
 }
 
-export const getUsers = async () => {
+const getUsers = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: 'force-cache' });
   return res.json();
 }
